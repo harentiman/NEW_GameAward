45257acd969592473a7bd22581fae0381f4f5046
harentiman <harentiman@gmail.com>
2020-05-18 22:00:06 +0900

    マスタ更新。 マスタ更新、Tutorialの追加。 マスタ更新、Stage-1-1を修正。

---
+++ SIN_GameAward/Assets/C#/Tutorial_UI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// 自動で消える
+public class Tutorial_UI : MonoBehaviour
+{
+
+    public float timer = 5.0f;
+    void Start()
+    {
+    
+    }
+
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            Destroy(gameObject, timer);
+        }
+    }
+
+}
