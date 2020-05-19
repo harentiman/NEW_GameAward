05ae3895ff4ef881dc9643b7c1ebee9cf001b244
harentiman <harentiman@gmail.com>
2020-05-19 22:56:05 +0900

    マスタ更新。 マスタ更新、ゲーム終了処理を追加。

---
+++ SIN_GameAward/Assets/C#/Game_Exit.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Game_Exit : MonoBehaviour
+{
+
+    public Button button;
+
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnClick()
+    {
+        Debug.Log("ゲーム終了");
+
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #elif UNITY_WEBPLAYER
+		        Application.OpenURL("http://www.yahoo.co.jp/");
+        #else
+		        Application.Quit();
+        #endif
+    }
+
+}
