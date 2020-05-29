991e303a29b88712486c6bd23c86ec320dbad55a
harentiman <harentiman@gmail.com>
2020-05-30 03:00:00 +0900

    マスタ更新、デバッグ用強制終了Script追加。

---
+++ SIN_GameAward/Assets/C#/Scene_Move/Debug_END.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Debug_END : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            FadeManager.Instance.LoadScene("StageSelect", 2);
+        }
+    }
+
+}
