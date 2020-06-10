e300a5b2d3087103632e41fcfaf5f61f71b05e09
yuma0000 <gdgdzzz@outlook.jp>
2020-06-10 12:29:58 +0900

    キューブ着地SE追加

---
+++ SIN_GameAward/Assets/C#/Objects_Script/cubeSE.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeSE : MonoBehaviour
+{
+    public AudioClip sound4;
+    AudioSource AudioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            {
+                AudioSource.PlayOneShot(sound4);
+            }
+        }
+    }
+}
